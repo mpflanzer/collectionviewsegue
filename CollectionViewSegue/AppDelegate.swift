47b3167f323692285980dd4053e4088e2ca7c532
Moritz Pflanzer <moritz@pflanzer.eu>
2018-09-02 16:39:05 +0200

    Initial Commit

---
+++ CollectionViewSegue/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CollectionViewSegue
+//
+//  Created by Moritz Pflanzer on 02.09.18.
+//  Copyright © 2018 Moritz Pflanzer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CollectionViewSegue/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CollectionViewSegue
+//
+//  Created by Moritz Pflanzer on 02.09.18.
+//  Copyright © 2018 Moritz Pflanzer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
